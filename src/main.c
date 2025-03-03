c7387fa010458ed8b17e03cfbfacbfe613771d7c
Oliver Voogd <olivervoogd@gmail.com>
2025-03-03 13:25:33 +1100

    basic chunk assembling / disassembling

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
+// Can we write a makefile for this and
+// compile and run the whole project on WSL?
 int main(int argc, const char *argv[])
 {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
     return 0;
 }
