e1698b36c9b2f2e9e92230dbae4650ff0cc91729
Oliver Voogd <olivervoogd@gmail.com>
2025-03-04 14:59:25 +1100

    finished chapter 'A Virtual Machine'

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "vm.h"
 #include "chunk.h"
 #include "debug.h"
 
@@ -6,16 +7,31 @@
 // compile and run the whole project on WSL?
 int main(int argc, const char *argv[])
 {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
