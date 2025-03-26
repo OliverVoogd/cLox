a5930070388e5e242f277bdf940820e494f77336
Oliver Voogd <olivervoogd@gmail.com>
2025-03-26 17:42:48 +1100

    added abs native func

--- src/native.c
+++ src/native.c
@@ -1,6 +1,9 @@
 #include "native.h"
 
 #include <time.h>
+#include <stdlib.h>
+
+#include "value.h"
 
 static runtimeErrorFn runtimeError;
 
@@ -10,14 +13,33 @@ static Value clockNative(int argCount, Value *args, bool *success)
     {
         runtimeError("Expected 0 arguments but got %d.", argCount);
         *success = false;
-        return NUMBER_VAL(0);
+        return NIL_VAL;
     }
     *success = true;
     return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
+
+static Value absNative(int argCount, Value *args, bool *success)
+{
+    if (argCount != 1)
+    {
+        runtimeError("Expected 1 argument but got &d.", argCount);
+        *success = false;
+        return NIL_VAL;
+    }
+    if (!IS_NUMBER(args[0]))
+    {
+        runtimeError("Expected a number to 'abs' native function.");
+        *success = false;
+        return NIL_VAL;
+    }
+    *success = true;
+    return NUMBER_VAL(abs(AS_NUMBER(args[0])));
 }
 
 void defineAllNatives(defineNativeFn defineNative, runtimeErrorFn rError)
 {
     runtimeError = rError;
     defineNative("clock", clockNative);
+    defineNative("abs", absNative);
 }
